b8539652b7984e50b2c013a91758ccb301091d93
Bansi <bansivachhani153@gmail.com>
2025-04-15 21:01:25 +0530

    add user account and management

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,16 +1,17 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({
-    subscriber:{
-        type: Schema.Types.ObjectId,//one who is subscribing
-        ref: "User",
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, //one who is subscribing
+      ref: "User",
     },
-    channel:{
-        type: Schema.Types.ObjectId,//one to whom subscribe by subscriber
-        ref: "User",
+    channel: {
+      type: Schema.Types.ObjectId, //one to whom subscribe by subscriber
+      ref: "User",
     },
-    
-
-})
+  },
+  { timestamps: true }
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
